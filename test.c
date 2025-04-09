c416100f9e6f85021fdefe477a14b387916366ca
vietanhtbdh17 <vietanh160103@gmail.com>
2025-04-09 12:18:43 +0700

    tiep

--- test.c
+++ test.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
+#include<math.h>
 
 int main(){
-    int a = 100000, b = 2000000;
-    long long tich = 1ll * a * b;
-    printf("Tich = %lld\n", tich);
+    int a = 100;
+    double b = sqrt(a);
+    int c = pow(a, 2);
+    printf("The square root of %d is %.2f\n", a, b);
+    printf("The power of %d to 2 is %d\n", a, c);
     return 0;
 }
